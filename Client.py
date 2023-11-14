b132553c57921b311c1674b5685caf2e7eb6d940
Alex Itzenhuiser <alexitzenhuiser@gmail.com>
2023-11-14 16:15:44 -0600

    updated main

--- Client.py
+++ Client.py
@@ -5,8 +5,8 @@ import threading
 HOST = '172.17.9.51'
 PORT = 65432
 
-def format_message(message_type, message_body):
-    message_header = f"{message_type}:{len(message_body)}"
+def format_message(message_type, username, message_body):
+    message_header = f"{message_type}:{username}:{len(message_body)}"
     return f"{message_header}|{message_body}"
 
 
@@ -15,17 +15,18 @@ def receive_messages(sock):
         try:
             message = sock.recv(1024).decode('utf-8')
             if message:
-                print(message)
+                _ ,mess_to_print = message.split("|")
+                print(mess_to_print)
         except:
             print("An error occurred.")
             sock.close()
             break
 
-def write_messages(sock):
+def write_messages(sock, username):
     while True:
         message = input("")
         # When sending a message
-        formatted_message = format_message("MSG", message)
+        formatted_message = format_message("MSG", username, message)
         sock.send(formatted_message.encode('utf-8'))
 
 
@@ -37,13 +38,13 @@ def start_client():
     print(f"Handshake message from server: {handshake_response}")
 
     screen_name = input("Enter your screen name: ")
-    name_message = format_message("NAME", screen_name)
+    name_message = format_message("NAME", screen_name, "")
     client_socket.send(name_message.encode('utf-8'))
 
     receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
     receive_thread.start()
 
-    write_thread = threading.Thread(target=write_messages, args=(client_socket,))
+    write_thread = threading.Thread(target=write_messages, args=(client_socket, screen_name))
     write_thread.start()
 
 # Start the client
